75a8e446ec8677f97794bb7c3710d252948f0653
bao48 <31197028+bao48@users.noreply.github.com>
2017-11-23 20:19:36 -0500

    Create Pigs.java

---
+++ Pigs.java
@@ -0,0 +1,64 @@
+// This is the main code to translate english into pig latin!
+
+public class Pigs {
+
+    private final String tidbit;
+    private String[] piggy;
+    
+    // a bunch of constructors --> overloaded, but we're only gonna have one type of pig here, you can also put multiple paramerters in a constructor
+    public Pigs(String phrase){
+        tidbit = phrase;
+        translate();
+    }
+
+    private void translate() {
+        String[] segs = tidbit.split(" ");
+        piggy = new String[segs.length];
+        for (int i=0; i<=segs.length-1; i++) {
+            if ((segs[i].substring(0, 1).equals("a")) || 
+                (segs[i].substring(0, 1).equals("e")) || 
+                (segs[i].substring(0, 1).equals("i")) || 
+                (segs[i].substring(0, 1).equals("o")) || 
+                (segs[i].substring(0, 1).equals("u"))) {
+                if ((segs[i].substring(segs[i].length()-1).equals("!")) ||
+                    (segs[i].substring(segs[i].length()-1).equals(".")) ||
+                    (segs[i].substring(segs[i].length()-1).equals("?"))) {
+                        piggy[i] = segs[i].substring(0, segs[i].length()-1) + "way" + segs[i].substring(segs[i].length()-1);
+                } else {
+                    piggy[i] = segs[i] + "way";
+                }
+            } else if (!(segs[i].substring(1, 2).equals("a")) &&
+                       !(segs[i].substring(1, 2).equals("e")) && 
+                       !(segs[i].substring(1, 2).equals("i")) && 
+                       !(segs[i].substring(1, 2).equals("o")) && 
+                       !(segs[i].substring(1, 2).equals("u"))) {
+                if ((segs[i].substring(segs[i].length()-1).equals("!")) ||
+                    (segs[i].substring(segs[i].length()-1).equals(".")) ||
+                    (segs[i].substring(segs[i].length()-1).equals("?"))) {
+                    piggy[i] = segs[i].substring(2, segs[i].length()-1) + segs[i].substring(0, 2) + "ay" + segs[i].substring(segs[i].length()-1);
+                } else {
+                    piggy[i] = segs[i].substring(2) + segs[i].substring(0, 2) + "ay";
+                }
+
+            } else {
+                if ((segs[i].substring(segs[i].length()-1).equals("!")) ||
+                    (segs[i].substring(segs[i].length()-1).equals(".")) ||
+                    (segs[i].substring(segs[i].length()-1).equals("?"))) {
+                    piggy[i] = segs[i].substring(1, segs[i].length()-1) + segs[i].substring(0, 1) + "ay" + segs[i].substring(segs[i].length()-1);
+                } else {
+                    piggy[i] = segs[i].substring(1) + segs[i].substring(0, 2) + "ay";
+                }
+            }
+            
+        }
+    }
+
+
+    
+    public void translatePrint() {
+        for (int i=0; i<= piggy.length-1; i++){
+            System.out.print(piggy[i] + " ");
+        }
+    }
+       
+}
